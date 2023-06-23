c8dd95db3a307731a8be545ee11ba5090b008c91
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-06-23 15:11:38 +0530

    store data in redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import { fetchData } from "./utils/api";
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
-  return (
-      <div className='App'>
-        App
-      </div>
-  )
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    fetchData("/movie/popular").then((res)=>{
+      console.log(res);
+      dispatch(getApiConfiguration(res))
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+      
+  };
+
+  
+
+
+  return <div className="App">App</div>;
 }
 
-export default App
+export default App;
