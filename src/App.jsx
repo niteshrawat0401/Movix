d412b9a950737d3b325b189901c97ab628a2b2db
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-06-26 16:50:40 +0530

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,50 @@
 import { useState, useEffect } from "react";
 import { fetchData } from "./utils/api";
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFoud from "./pages/404/PageNotFound";
 
 function App() {
-  const dispatch = useDispatch()
-
+  const { url } = useSelector((state) => state.home);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     getData();
   }, []);
 
   const getData = () => {
-    fetchData("/movie/popular").then((res)=>{
-      console.log(res);
-      dispatch(getApiConfiguration(res))
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-      
+    fetchData("/movie/popular")
+      .then((res) => {
+        console.log(res);
+        dispatch(getApiConfiguration(res));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  
-
-
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="*" element={<PageNotFoud />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
