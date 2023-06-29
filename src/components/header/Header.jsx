354f9b00d5d62d0a0ef48f5e48fe5de8eab1013a
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-06-29 14:32:58 +0530

    banner css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.scss'
 
 const Header = () => {
+  const [show, setShow] = useState('top');
+  const [lastScrolly, setLastScrolly] = useState(0)
+  const [mobileMenu, setMobileMenu] = useState(false);
+  
   return (
     <div>Header</div>
   )
