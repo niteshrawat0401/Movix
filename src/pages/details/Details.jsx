fede27c23feee50f167c198c4c2b3eb174a4f694
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-07-03 14:00:58 +0530

    add playbtn, detailsbanner, details

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './style.scss'
+import useFetch from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
+import DetailsBanner from './detailBanner/DeatailBanner'
 
 const Details = () => {
+  // const { mediaType, id } = useParams();
+  // console.log(mediaType, id);
+  // const {data, loading} = useFetch(`/${mediaType}/${id}`)
   return (
-    <div>Details</div>
+    <div>
+      <DetailsBanner/>
+    </div>
   )
 }
 
