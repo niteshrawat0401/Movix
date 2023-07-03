8fdddb7bead067ebbdf3522cfd94564e1378cfd2
niteshrawat0401 <niteshrawat0401@gmail.com>
2023-07-03 11:58:52 +0530

    popular, trendind and toprated carousel

--- src/components/genres/Genres.jsx
+++ src/components/genres/Genres.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import './style.scss'
 
 const Genres = ({data}) => {
     const {genres} = useSelector((state) => state.home)
-  return (
-    <div className='genres'>
+    return (
+        <div className='genres'>
         {data?.map((g)=>{
-            return
+            if(!genres[g]?.name)
+            return;
+            return (
+                <div key={g} className='genre'>
+                    {genres[g]?.name}
+                </div>
+            )
         })}
     </div>
   )
